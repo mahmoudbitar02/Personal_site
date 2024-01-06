219cfbf0647290ad421e6f5c57eaa488c1a792c6
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2024-01-06 17:43:29 +0100

    work on models

--- home/models.py
+++ home/models.py
@@ -10,6 +10,9 @@ class Home (models.Model):
     job = models.CharField(max_length=50)
     image = models.ImageField(upload_to='home/', null=True,blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class About (models.Model):
     name =models.CharField(max_length=20)
@@ -19,9 +22,14 @@ class About (models.Model):
     image = models.ImageField(upload_to='about/')
     aboutme = models.TextField(max_length=20000)
 
+    def __str__(self):
+        return self.name
+
 class Skill (models.Model):
     skill = models.CharField(max_length=15)
     percentage = models.IntegerField()
+    def __str__(self):
+        return self.skill
 
 
 class Service (models.Model):
@@ -30,6 +38,8 @@ class Service (models.Model):
     icon2 = models.CharField(max_length=20)
     job = models.CharField(max_length=50)
     subtitle = models.CharField(max_length=500)
+    def __str__(self):
+        return self.job
 
 
 class Portfoilo(models.Model):
@@ -38,7 +48,8 @@ class Portfoilo(models.Model):
     text = models.CharField(max_length=50)
     job = models.CharField(max_length=50)
     date = models.DateField(default=1, null =True,blank=True)
-
+    def __str__(self):
+        return self.title      
 
 class Blog (models.Model):
     title = models.CharField(max_length=200)
@@ -48,8 +59,12 @@ class Blog (models.Model):
     subtitle = models.CharField(max_length=500)
     author = models.ForeignKey(User, related_name ='blog_author', on_delete = models.CASCADE)
     auth_image = models.ImageField(upload_to='auth/')
+    def __str__(self):
+        return self.title
 
 
 
 class Cattigory (models.Model):
     name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
